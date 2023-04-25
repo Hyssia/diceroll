ba29837df76ad4d78cb8141477326e2ae5e0f183
Anders Bredesen <nexpex@hotmail.com>
2023-04-25 12:52:45 +0200

    Updates

--- script.js
+++ script.js
@@ -23,15 +23,33 @@ const playerNameTwo = document.getElementById('name--1');
 playerNameOne.textContent = player1Name;
 playerNameTwo.textContent = player2Name;
 
-// starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+//  Init variables
+let scores;
+let currentScore;
+let activePlayer;
+let playing;
 
-let scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// starting conditions
+const init = function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  activePlayer = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  scores = [0, 0];
+  currentScore = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+};
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -40,6 +58,9 @@ const switchPlayer = function () {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
+
+// Initialization
+init();
 
 // rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -74,7 +95,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
     // 2. check if score is >= 100
     // finish the game
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
       document
@@ -91,18 +112,5 @@ btnHold.addEventListener('click', function () {
 });
 
 btnNew.addEventListener('click', function () {
-  playing = true;
-  activePlayer = 0;
-  document.querySelector('.player--0').classList.remove('player--winner');
-  document.querySelector('.player--1').classList.remove('player--winner');
-  document
-    .querySelector(`.player--${activePlayer}`)
-    .classList.add('player--active');
-
-  scores = [0, 0];
-  currentScore = 0;
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
+  init();
 });
